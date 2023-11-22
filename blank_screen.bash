dea992a6203b2510975486d5aa9662cb3ba29c0e
David McCall <3423876+davidgmccall@users.noreply.github.com>
2023-11-22 09:58:47 -0700

    Script and blank image

---
+++ blank_screen.bash
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+scriptDir="${0%/*}"
+blankSecs=10
+blankImage=$(readlink -e ${scriptDir}/black-512x512.png)
+blankPid=
+keysLog=$(mktemp --tmpdir=/tmp keyd-XXXXXXXX.log)
+keysTime=$(mktemp --tmpdir=/tmp keyd-XXXXXXXX.time)
+
+trap cleanup EXIT SIGTERM SIGKILL SIGABRT
+function cleanup () {
+    sudo pkill -ef '^keyd -m'
+    if [[ ${blankPid} ]] ; then
+        kill ${blankPid}
+    fi
+    rm -fv ${keysLog}
+    rm -fv ${keysTime}
+    reset
+}
+
+sudo keyd -m &> ${keysLog} &
+reset
+
+isBlank=0
+while sleep 1; do
+    if tail -n 2 ${keysLog} | grep -q 'q down' &>/dev/null ; then
+        exit
+    fi
+
+    if (( ! isBlank )) ; then
+        touch --date="${blankSecs} secs ago" ${keysTime}
+    fi
+
+    if [[ $isBlank == 0 && ${keysLog} -ot ${keysTime} ]] ; then
+        pqiv --fullscreen --hide-info-box --fade ${blankImage} &
+        blankPid=$!
+        isBlank=1
+    elif [[ $isBlank == 1 && ${keysLog} -nt ${keysTime} ]] ; then
+        if [[ ${blankPid} ]] ; then
+            kill ${blankPid}
+            blankPid=
+        fi
+        isBlank=0
+    fi
+    echo
+done
+
